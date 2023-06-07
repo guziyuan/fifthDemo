c9a3028300c2dd09ed5c55ac3ac70e1bb1b2880c
guziyuan <132895909+guziyuan@users.noreply.github.com>
2023-06-07 19:02:28 +0800

    Add files via upload

---
+++ Blockchain.js
@@ -0,0 +1,42 @@
+import UTXOPool from './UTXOPool.js'
+
+Blockchain
+class Blockchain {
+  // 1. 完成构造函数及其参数
+  /* 构造函数需要包含
+      - 名字
+      - 创世区块
+      - 存储区块的映射
+  */
+  constructor() {}
+
+  // 2. 定义 longestChain 函数
+  /*
+    返回当前链中最长的区块信息列表
+  */
+  longestChain() {
+    return []
+  }
+
+  // 判断当前区块链是否包含
+  containsBlock(block) {
+    // 添加判断方法
+    return false
+  }
+
+  // 获得区块高度最高的区块
+  maxHeightBlock() {
+    // return Block
+  }
+
+  // 添加区块
+  /*
+
+  */
+  _addBlock(block) {
+    if (!block.isValid()) return
+    if (this.containsBlock(block)) return
+  }
+}
+
+export default Blockchain
